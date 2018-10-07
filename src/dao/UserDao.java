edafd2ff4ed3a2695f760f97581f4de08223034c
jmck074 <jmck074@aucklanduni.ac.nz>
2018-10-08 11:30:55 +1300

    Deleted extra POJO folder and files

--- src/dao/UserDao.java
+++ src/dao/UserDao.java
@@ -2,7 +2,7 @@ package dao;
 
 import pojo.User;
 
-public interface UserDao{
+public interface UserDao {
     public boolean login(String name, String UserPasswd);
     public boolean register(User user);
     public boolean delete(int id);
--- src/dao/UserDaoImp.java
+++ src/dao/UserDaoImp.java
@@ -2,6 +2,9 @@ package dao;
 
 import pojo.User;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class UserDaoImp implements UserDao {
     @Override
     public boolean login(String name, String UserPasswd) {
@@ -9,10 +12,25 @@ public class UserDaoImp implements UserDao {
     }
 
     @Override
-    public boolean register(User user) {
-        return false;
-    }
+    public boolean register(User user) throws SQLException {
+        //add user add password to user table
+        try (PreparedStatement stmt = this.conn.prepareStatement("INSERT INTO user(UserName, UserPasswd, profilePath) VALUES (?,?,?);")) {
+            stmt.setString(1, User.getName());
+            stmt.setString(2, User.getPassword());
+//TODO check about profile path...
+            stmt.setString(3,"?");
+            stmt.executeUpdate();
+        }
+        // add user and info to information table
+        try(PreparedStatement stmt=this.conn.prepareStatement("INSERT INTO userinformation(NickName, RealName, Birthday, Country) VALUES (?,?,?,?);")){
+            stmt.setString(1,thisUser.getName());
+            stmt.setString(2,thisUser.getRealName());
+            stmt.setInt(3, thisUser.getBirthday());
+            stmt.setString(4,thisUser.getCountry());
+            stmt.executeUpdate();
+        }
 
+    }
     @Override
     public boolean delete(int id) {
         return false;
