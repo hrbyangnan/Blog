277d5ca07747a56f8b8f90694837ce0d12268772
Neco Yang <nyan779@aucklanduni.ac.nz>
2018-10-14 00:09:18 +1300

    temp

--- src/UserServlet/RegisterUserServlet.java
+++ src/UserServlet/RegisterUserServlet.java
@@ -9,19 +9,47 @@ package UserServlet;
 
 import dao.UserDao;
 import dao.UserDaoImp;
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import pojo.User;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.sql.SQLException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.List;
 
 
 public class RegisterUserServlet extends HttpServlet {
+    private File uploadsFolder;
+    private File tempFolder;
+
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        initUploadFolder();
+    }
+
+    public void initUploadFolder() {
+        this.uploadsFolder = new File(getServletContext().getRealPath("/Uploaded_Profile"));
+        if (!uploadsFolder.exists()) {
+            uploadsFolder.mkdirs();
+        }
+
+        this.tempFolder = new File(getServletContext().getRealPath("/WEB-INF/temp"));
+        if (!tempFolder.exists()) {
+            tempFolder.mkdirs();
+        }
+    }
+
     public void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         doPost(request, response);
@@ -31,19 +59,65 @@ public class RegisterUserServlet extends HttpServlet {
             throws ServletException, IOException {
 
         System.out.println("Check that servlet doPost is loading");
+        DiskFileItemFactory factory = new DiskFileItemFactory();
+        factory.setSizeThreshold(4 * 1024);
+        factory.setRepository(tempFolder);
+        ServletFileUpload upload = new ServletFileUpload(factory);
 
-        String userName = request.getParameter("username");
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
 
-        String firstName = request.getParameter("firstName");
-        String lastName = request.getParameter("lastName");
+        String userName = null;
+        String firstName = null;
+        String lastName = null;
 
-        String realName = firstName + " " + lastName;
-        String password = request.getParameter("password");
-        String country = request.getParameter("country");
+        String password = null;
+        String country = null;
+        String birthday = null;
+        String fileName = null;
+        String realName = null;
 
-        String birthday=request.getParameter("birthday");
+        try {
+            List<FileItem> fileItems = upload.parseRequest(request);
+            File fullsizeImagefile = null;
+
+
+            for (FileItem fi : fileItems) {
+                if (!fi.isFormField()) {
+                    fileName = fi.getName();
+                    fullsizeImagefile = new File(uploadsFolder, fileName);
+                    fi.write(fullsizeImagefile);
+                } else if (fi.getFieldName() != null) {
+                    if (fi.getFieldName().equals("username")) {
+                        userName = fi.getString();
+                    }
+                    if (fi.getFieldName().equals("firstName")) {
+                        firstName = fi.getString();
+                    }
+                    if (fi.getFieldName().equals("lastName")) {
+                        lastName = fi.getString();
+                    }
+                    if (fi.getFieldName().equals("password")) {
+                        password = fi.getString();
+                    }
+                    if (fi.getFieldName().equals("country")) {
+                        country = fi.getString();
+                    }
+                    if (fi.getFieldName().equals("birthday")) {
+                        birthday = fi.getString();
+                    }
+                    realName = firstName + " " + lastName;
+                }
+            }
+
+        } catch (Exception e) {
+            throw new ServletException(e);
+        }
+
         SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
         java.util.Date d = null;
+
+        String picPath = "/Uploaded_Profile/" + fileName;
 
         try {
             d = format.parse(birthday);
@@ -52,7 +126,6 @@ public class RegisterUserServlet extends HttpServlet {
         }
 
         java.sql.Date date = new java.sql.Date(d.getTime());
-
 
 
         String email = request.getParameter("email");
